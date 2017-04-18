83256f4c337bec2a6fab71f1a3043f0bc40fecd3
Ramiz Raja <informramiz@gmail.com>
2017-04-19 04:14:55 +0500

    feat: Tools class with common utility functions

---
+++ src/tools.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    std::cout << "calculateRMSE() - Error - Invalid Estimations vector size" << std::endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  VectorXd sum(4);
+  sum << 0, 0, 0, 0;
+
+  for(int i = 0; i < estimations.size(); ++i) {
+    VectorXd estimated = estimations[i];
+    VectorXd actual = ground_truth[i];
+
+    VectorXd diff = (estimated - actual);
+    //coefficient-wise multiplication
+    diff = diff.array() * diff.array();
+    sum = sum + diff;
+  }
+
+  //calculate the mean
+  Eigen::VectorXd mean = sum / estimations.size();
+
+  //calculate the squared root
+  rmse = mean.array().sqrt();
+
+  return rmse;
+}
+
+float Tools::NormalizeAngle(float angle_rad) {
+  //angle normalization
+    while (angle_rad > M_PI)
+      angle_rad -= 2.*M_PI;
+
+    while (angle_rad < -M_PI)
+      angle_rad += 2. * M_PI;
+
+    return angle_rad;
+}
---
+++ src/tools.h
@@ -0,0 +1,30 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+
+class Tools {
+public:
+  /**
+  * Constructor.
+  */
+  Tools();
+
+  /**
+  * Destructor.
+  */
+  virtual ~Tools();
+
+  /**
+  * A helper method to calculate RMSE.
+  */
+  static Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
+
+  /**
+   * A helper method to normalize angle between [-pi, pi]
+   */
+  static float NormalizeAngle(float angle_rad);
+
+};
+
+#endif /* TOOLS_H_ */
